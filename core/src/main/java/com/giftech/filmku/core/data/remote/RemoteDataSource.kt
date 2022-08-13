f0667b1823c1b0462625efb5623b65ae12cdf66a
galihif <galihindra2401@gmail.com>
2022-08-13 10:08:49 +0700

    build: hide base url and api key in local
    Signed-off-by: galihif <galihindra2401@gmail.com>

--- core/src/main/java/com/giftech/filmku/core/data/remote/RemoteDataSource.kt
+++ core/src/main/java/com/giftech/filmku/core/data/remote/RemoteDataSource.kt
@@ -1,12 +1,14 @@
 package com.giftech.filmku.core.data.remote
 
+import com.giftech.filmku.core.BuildConfig
 import com.giftech.filmku.core.data.remote.network.ApiService
+import com.giftech.filmku.core.utils.Constants
 import com.giftech.filmku.core.utils.DummyData
 import javax.inject.Inject
 
 class RemoteDataSource @Inject constructor(
     private val api:ApiService
 ) {
-    fun getTest() = "fuckckkkk"
+    fun getTest() = "Constants.API_KEY"
     suspend fun getNowPlaying() = api.getNowPlaying()
 }
---
+++ core/src/main/java/com/giftech/filmku/core/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.giftech.filmku.core.utils
+
+import com.giftech.filmku.core.BuildConfig
+
+object Constants {
+    const val BASE_URL = BuildConfig.BASE_URL
+    const val API_KEY = BuildConfig.API_KEY
+}
