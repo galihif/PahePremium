82471421ce8546f925440733dd232324738442e9
galihif <galihindra2401@gmail.com>
2022-08-13 09:30:31 +0700

    di: provide retrofit api service
    Signed-off-by: galihif <galihindra2401@gmail.com>

--- core/src/main/java/com/giftech/filmku/core/di/RemoteModule.kt
+++ core/src/main/java/com/giftech/filmku/core/di/RemoteModule.kt
@@ -1,16 +1,43 @@
 package com.giftech.filmku.core.di
 
 import com.giftech.filmku.core.data.remote.RemoteDataSource
+import com.giftech.filmku.core.data.remote.network.ApiService
+import com.giftech.filmku.core.utils.Constants
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 class RemoteModule {
+
+    @Provides
+    fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+            .connectTimeout(120, TimeUnit.SECONDS)
+            .readTimeout(120, TimeUnit.SECONDS)
+            .build()
+    }
+
+    @Provides
+    fun provideApiService(client: OkHttpClient): ApiService {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+        return retrofit.create(ApiService::class.java)
+    }
+
     @Provides
     @Singleton
-    fun provideRemote() = RemoteDataSource()
+    fun provideRemote(apiService: ApiService) = RemoteDataSource(apiService)
 }
