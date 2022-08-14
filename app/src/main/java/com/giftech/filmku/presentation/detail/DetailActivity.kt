3beabd81ec23823eec9766d74e2d4e99a6c52e00
galihif <galihindra2401@gmail.com>
2022-08-14 15:50:30 +0700

    ui: slicing activity_detail.xml
    Signed-off-by: galihif <galihindra2401@gmail.com>

--- app/src/main/java/com/giftech/filmku/presentation/detail/DetailActivity.kt
+++ app/src/main/java/com/giftech/filmku/presentation/detail/DetailActivity.kt
@@ -3,10 +3,14 @@ package com.giftech.filmku.presentation.detail
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.giftech.filmku.R
+import com.giftech.filmku.databinding.ActivityDetailBinding
 
 class DetailActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityDetailBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
