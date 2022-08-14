45fc96de83b81fbfacc4813c8b5e0c71ef7df15c
galihif <galihindra2401@gmail.com>
2022-08-14 14:40:47 +0700

    feat: add pkg detail activity
    Signed-off-by: galihif <galihindra2401@gmail.com>

---
+++ app/src/main/java/com/giftech/filmku/presentation/detail/DetailActivity.kt
@@ -0,0 +1,12 @@
+package com.giftech.filmku.presentation.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.giftech.filmku.R
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
