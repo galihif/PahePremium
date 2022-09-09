2eb157c3ea1345cfd29962969ca227578f71b00e
galihif <galihindra2401@gmail.com>
2022-09-09 17:37:25 +0700

    feat: encrypt db with sql cipher
    Signed-off-by: galihif <galihindra2401@gmail.com>

--- core/src/main/java/com/giftech/filmku/core/di/LocalModule.kt
+++ core/src/main/java/com/giftech/filmku/core/di/LocalModule.kt
@@ -10,6 +10,8 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
 import javax.inject.Singleton
 
 @Module
@@ -18,11 +20,16 @@ class LocalModule {
 
     @Singleton
     @Provides
-    fun provideDatabase(@ApplicationContext context: Context): FilmDatabase =
-        Room.databaseBuilder(
+    fun provideDatabase(@ApplicationContext context: Context): FilmDatabase {
+        val passphrase: ByteArray = SQLiteDatabase.getBytes("giftech".toCharArray())
+        val factory = SupportFactory(passphrase)
+        return Room.databaseBuilder(
             context,
             FilmDatabase::class.java, "Film.db"
-        ).fallbackToDestructiveMigration().build()
+        ).fallbackToDestructiveMigration()
+            .openHelperFactory(factory)
+            .build()
+    }
 
     @Provides
     fun provideSavedMovieDao(database: FilmDatabase): SavedMovieDao = database.savedMovieDao()
