736d5e9bc6efcc22909520fd507f45e96d68c715
galihif <galihindra2401@gmail.com>
2022-09-12 14:35:31 +0700

    ui: hide keyboard when search clicked
    Signed-off-by: galihif <galihindra2401@gmail.com>

--- app/src/main/java/com/giftech/filmku/presentation/main/search/SearchFragment.kt
+++ app/src/main/java/com/giftech/filmku/presentation/main/search/SearchFragment.kt
@@ -1,14 +1,20 @@
 package com.giftech.filmku.presentation.main.search
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
 import com.giftech.filmku.databinding.FragmentSearchBinding
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class SearchFragment : Fragment() {
@@ -35,8 +41,29 @@ class SearchFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         if(activity != null){
-            binding.textDashboard.text = viewModel.msg
+            setOnSearch()
         }
+    }
+
+    private fun setOnSearch() {
+        binding.etSearch.setOnEditorActionListener { textView, actionId, keyEvent ->
+            if (actionId == EditorInfo.IME_ACTION_SEARCH){
+                searchMovie(textView.text.toString())
+                clearEditTextFocus()
+            }
+            true
+        }
+    }
+
+    private fun clearEditTextFocus() {
+        binding.etSearch.clearFocus()
+        val imm: InputMethodManager =
+            activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(binding.etSearch.windowToken, 0)
+    }
+
+    private fun searchMovie(query: String) {
+        Toast.makeText(activity, query, Toast.LENGTH_SHORT).show()
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/giftech/filmku/presentation/main/search/SearchViewModel.kt
+++ app/src/main/java/com/giftech/filmku/presentation/main/search/SearchViewModel.kt
@@ -3,7 +3,10 @@ package com.giftech.filmku.presentation.main.search
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import com.giftech.filmku.core.domain.model.Movie
 import com.giftech.filmku.core.domain.usecase.FilmUseCase
+import com.giftech.filmku.core.utils.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
@@ -11,5 +14,6 @@ import javax.inject.Inject
 class SearchViewModel @Inject constructor(
     private val useCase: FilmUseCase
 ): ViewModel() {
-    val msg = useCase.getTest()
+    fun getSearchResult(query:String):LiveData<Resource<List<Movie>>> =
+        useCase.getSearchResult(query).asLiveData()
 }
