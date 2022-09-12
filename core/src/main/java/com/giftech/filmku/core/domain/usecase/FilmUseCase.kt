eeb463b20615cf3add1bc368c482f46cdcf1f8aa
galihif <galihindra2401@gmail.com>
2022-09-12 16:16:18 +0700

    feat: add method to get movie results from api based on query
    Signed-off-by: galihif <galihindra2401@gmail.com>

--- core/src/main/java/com/giftech/filmku/core/domain/usecase/FilmUseCase.kt
+++ core/src/main/java/com/giftech/filmku/core/domain/usecase/FilmUseCase.kt
@@ -5,15 +5,15 @@ import com.giftech.filmku.core.utils.Resource
 import kotlinx.coroutines.flow.Flow
 
 interface FilmUseCase {
-    fun getTest():String
+    fun getNowPlaying(): Flow<Resource<List<Movie>>>
+    fun getPopular(): Flow<Resource<List<Movie>>>
+    fun getMovie(movieId: Int): Flow<Resource<Movie>>
 
-    fun getNowPlaying():Flow<Resource<List<Movie>>>
-    fun getPopular():Flow<Resource<List<Movie>>>
-    fun getMovie(movieId:Int):Flow<Resource<Movie>>
+    fun getSearchResult(query: String): Flow<Resource<List<Movie>>>
 
     suspend fun addMovieToWatchList(movie: Movie)
-    suspend fun isMovieInWatchList(movieId:Int):Boolean
+    suspend fun isMovieInWatchList(movieId: Int): Boolean
     suspend fun removeMovieFromWatchlist(movieId: Int)
 
-    fun getWatchlist():Flow<List<Movie>>
+    fun getWatchlist(): Flow<List<Movie>>
 }
